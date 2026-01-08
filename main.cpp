95616238999b300f26f2df800935edbab9686171
Len Popp len.popp@gmail.com <len.popp@gmail.com>
2026-01-08 15:15:37 -0500

    just messin around

--- main.cpp
+++ main.cpp
@@ -151,7 +151,7 @@ static constexpr word_t allTargets[] = {
 // List of all permitted guess words
 static constexpr word_t allGuesses[] = {
 #include "words-target.h"
-#include "words-guess.h"
+//#include "words-guess.h"
 };
 
 static word_t getRandomTarget()
